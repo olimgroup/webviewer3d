a51851246d2f669f2652305e9530e07536188617
ethan <jwkim@olimplanet.com>
2021-12-30 14:46:45 +0900

    init playcanvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import React, { useRef, useState, useCallback, useEffect } from 'react';
 import './App.css';
 import * as pc from "playcanvas";
@@ -6,45 +7,53 @@ interface CanvasProps {
   height: number;
 }
 
+
+function Initialize(canvas: HTMLCanvasElement)
+{
+  const app = new pc.Application(canvas, {});
+  app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
+  app.setCanvasResolution(pc.RESOLUTION_AUTO);
+
+  // ensure canvas is resized when window changes size
+  window.addEventListener('resize', () => app.resizeCanvas());
+
+  // create box entity
+  const box = new pc.Entity('cube');
+  box.addComponent('model', {
+      type: 'box'
+  });
+  box.model?.material as pc.StandardMaterial;
+  box.on('update', dt => {
+    console.log(dt);
+  });
+  app.root.addChild(box);
+
+  // create camera entity
+  const camera = new pc.Entity('camera');
+  camera.addComponent('camera', {
+      clearColor: new pc.Color(0.12, 0.11, 0.15)
+  });
+  app.root.addChild(camera);
+  camera.setPosition(0, 0, 5);
+
+  // create directional light entity
+  const light = new pc.Entity('light');
+  light.addComponent('light');
+  app.root.addChild(light);
+  light.setEulerAngles(45, 0, 0);
+
+  // rotate the box according to the delta time since the last frame
+  app.on('update', dt => box.rotate(10 * dt, 20 * dt, 30 * dt));
+
+  app.start();
+}
+
 function App({ width, height }: CanvasProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  if (canvasRef.current)
-  {
-    const app = new pc.Application(canvasRef.current, {});
-    app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
-        app.setCanvasResolution(pc.RESOLUTION_AUTO);
-
-        // ensure canvas is resized when window changes size
-        window.addEventListener('resize', () => app.resizeCanvas());
-
-        // create box entity
-        const box = new pc.Entity('cube');
-        box.addComponent('model', {
-            type: 'box'
-        });
-        app.root.addChild(box);
-
-        // create camera entity
-        const camera = new pc.Entity('camera');
-        camera.addComponent('camera', {
-            clearColor: new pc.Color(0.1, 0.1, 0.1)
-        });
-        app.root.addChild(camera);
-        camera.setPosition(0, 0, 3);
-
-        // create directional light entity
-        const light = new pc.Entity('light');
-        light.addComponent('light');
-        app.root.addChild(light);
-        light.setEulerAngles(45, 0, 0);
-
-        // rotate the box according to the delta time since the last frame
-        app.on('update', dt => box.rotate(10 * dt, 20 * dt, 30 * dt));
-
-        app.start();
-
-  }
-  
+  useEffect(()=>{
+    if (canvasRef.current)
+        Initialize(canvasRef.current);
+  });
   return (
     <div className="App">
       <canvas ref={canvasRef} height={height} width={width} className="canvas"/>
