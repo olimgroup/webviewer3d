61f0c330cafc1c3d258f3dcccc99edc52143e218
ethan <jwkim@olimplanet.com>
2021-12-30 13:23:32 +0900

    init playcanvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
 import './App.css';
+import * as pc from "playcanvas";
+interface CanvasProps {
+  width: number;
+  height: number;
+}
 
-function App() {
+function App({ width, height }: CanvasProps) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  if (canvasRef.current)
+  {
+    const app = new pc.Application(canvasRef.current, {});
+    app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
+        app.setCanvasResolution(pc.RESOLUTION_AUTO);
+
+        // ensure canvas is resized when window changes size
+        window.addEventListener('resize', () => app.resizeCanvas());
+
+        // create box entity
+        const box = new pc.Entity('cube');
+        box.addComponent('model', {
+            type: 'box'
+        });
+        app.root.addChild(box);
+
+        // create camera entity
+        const camera = new pc.Entity('camera');
+        camera.addComponent('camera', {
+            clearColor: new pc.Color(0.1, 0.1, 0.1)
+        });
+        app.root.addChild(camera);
+        camera.setPosition(0, 0, 3);
+
+        // create directional light entity
+        const light = new pc.Entity('light');
+        light.addComponent('light');
+        app.root.addChild(light);
+        light.setEulerAngles(45, 0, 0);
+
+        // rotate the box according to the delta time since the last frame
+        app.on('update', dt => box.rotate(10 * dt, 20 * dt, 30 * dt));
+
+        app.start();
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas ref={canvasRef} height={height} width={width} className="canvas"/>
     </div>
   );
 }
+
+App.defaultProps = {
+  width: 512,
+  height: 512
+};
 
 export default App;
